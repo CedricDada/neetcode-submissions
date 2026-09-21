bb78d1fba7639085c653b2607aa26fb2a14d9cb8
Cédric DADA <140360976+CedricDada@users.noreply.github.com>
2026-09-21 14:25:11 +0200

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        dp = [['' for i in range(len(s))] for j in range(len(s))]
+
+        for i in range(len(s)): 
+            dp[i][i] = s[i]
+        
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(i + 1, len(s)):
+                # Si les bords sont égaux ET que le centre est un palindrome parfait
+                if s[i] == s[j] and len(dp[i+1][j-1]) == j - i - 1:
+                    dp[i][j] = s[i] + dp[i+1][j-1] + s[j]
+                else:
+                    # Sinon, on transmet simplement le plus long palindrome trouvé jusque-là
+                    if len(dp[i+1][j]) >= len(dp[i][j-1]):
+                        dp[i][j] = dp[i+1][j]
+                    else:
+                        dp[i][j] = dp[i][j-1]
+                        
+        return dp[0][len(s)-1]
